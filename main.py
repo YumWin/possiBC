0a480a1d3f2fadb1ffa7caf2183457bdec10be19
YumWin <52228976+YumWin@users.noreply.github.com>
2020-07-15 18:01:31 +0800

    debug: for delete

--- main.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open('./source_file/sample.json','r',encoding='utf8')as fp:
-    json_data = json.load(fp)
-    print('这是文件中的json数据：',json_data)
-    print('这是读取到文件数据的数据类型：', type(json_data))
-    print(json_data['3544']['user_id'])
