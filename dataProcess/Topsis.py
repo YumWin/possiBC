26de7e9a7090559d2c9a49a3e7136f8b60c599c9
YumWin <52228976+YumWin@users.noreply.github.com>
2020-07-16 17:38:43 +0800

    feat: for TOPSIS

--- dataProcess/Topsis.py
+++ dataProcess/Topsis.py
@@ -1,8 +1,57 @@
 import json
+import numpy as np
+import pandas as pd
+from numpy import array
+import math
 
-info_dict={}
-def debugJudge():
-    todoList=[]
+# 进行标准化
+def metadataStandardize(data):
+    return data.apply(lambda x: ((x - np.min(x)) / (np.max(x) - np.min(x))))
+
+def get_informationEntropy(standardData,rows,cols,lnf,k):
+
+    # 矩阵计算并得到信息熵
+    standardData = array(standardData)
+    lnf = [[None] * cols for i in range(rows)]
+    lnf = array(lnf)
+    for i in range(0, rows):
+        for j in range(0, cols):
+            if standardData[i][j] == 0:
+                lnfij = 0.0
+            else:
+                p = standardData[i][j] / standardData.sum(axis=0)[j]
+                lnfij = math.log(p) * p * (-k)
+            lnf[i][j] = lnfij
+    lnf = pd.DataFrame(lnf)
+    return lnf
+
+def centropyWeightMethodCalculateWeight(metadata):
+
+    standardData = metadataStandardize(metadata)
+    # 计算出k值
+    rows = standardData.index.size
+    cols = standardData.columns.size
+    if(math.log(rows)==0):
+        k=1
+    else:
+        k = 1.0 / math.log(rows)
+    lnf = [[None] * cols for i in range(rows)]
+    # 计算并得到信息熵
+    informationEntropy = get_informationEntropy(standardData,rows,cols,lnf,k)
+    # 计算出冗余度
+    redundancy = 1 - informationEntropy.sum(axis=0)
+    # 计算各指标的权重
+    weights = [[None] * 1 for i in range(cols)]
+    for j in range(0, cols):
+        colweight= redundancy[j] / sum(redundancy)
+        weights[j] = colweight
+    # 计算各样本的综合得分并得到标准权重！！
+    weights = pd.DataFrame(weights)
+    return weights
+
+
+
+def dataPre():
     with open('./preData/debug.json','r',encoding='gbk')as fp:
         json_data = json.load(fp)
         # info_dict = {}
@@ -13,15 +62,121 @@ def debugJudge():
                 tempDict={}
                 tempDict['case_id']=case['case_id']
                 tempDict['case_type'] = case['case_type']
-                tempDict['upload_times']=case['upload_times']
                 addList.append(tempDict)
             info_dict[key]=addList
 
+        # info_json = json.dumps(info_dict,sort_keys=False, indent=4, separators=(',', ': '),ensure_ascii=False)
+        #
+        # print(type(info_json))
+        # f = open('E:\\possiBC\\deepData\\TopsisProcess.json', 'w')
+        # f.write(info_json)
+
+def findCase(dir,case_id):
+    with open(dir,'r',encoding='gbk')as fp:
+        findList=[]
+        json_data = json.load(fp)
+        for key in json_data:
+            for case in json_data[key]:
+                if(case['case_id']==case_id):
+                    case['user_id']=key
+                    findList.append(case)
+        return findList
+
+
+def debugJudge():
+    with open('./preData/debug.json','r',encoding='gbk')as fp:
+        readyList=[]
+        json_data = json.load(fp)
+        for key in info_dict:
+            caseList=list(info_dict[key])
+            for case in caseList:
+                if(case['case_id'] not in readyList):
+                    readyList.append(case['case_id'])
+                    findList=findCase('./preData/debug.json',case['case_id'])
+                    todoList=[]
+                    for element in findList:
+                        tempList=[]
+                        tempList.append(element['upload_times'])
+                        tempList.append(element['deltaScore'])
+                        tempList.append(element['deltaTime'])
+                        todoList.append(tempList)
+                    tempScoreList=standaradizing(positive(todoList,[0,1,0]))
+                    ScoreList=getScore(tempScoreList,getWeight(tempScoreList))
+                    i=0
+                    while(i<len(ScoreList)):
+                        writeInDict(findList[i]['user_id'],findList[i]['case_id'],'debugScore',ScoreList[i])
+                        i+=1
+        # info_json = json.dumps(info_dict,sort_keys=False, indent=4, separators=(',', ': '),ensure_ascii=False)
+        #
+        # print(type(info_json))
+        # f = open('E:\\possiBC\\deepData\\TopsisProcess.json', 'w')
+        # f.write(info_json)
+
+def firstConsJudge():
+    with open('./preData/firstConstruction.json','r',encoding='gbk')as fp:
+        readyList=[]
+        json_data = json.load(fp)
+        for key in info_dict:
+            caseList=list(info_dict[key])
+            for case in caseList:
+                if(case['case_id'] not in readyList):
+                    readyList.append(case['case_id'])
+                    findList=findCase('./preData/firstConstruction.json',case['case_id'])
+                    todoList=[]
+                    for element in findList:
+                        tempList=[]
+                        tempList.append(element['firstTime'])
+                        tempList.append(element['firstScore'])
+                        tempList.append(element['firstLines'])
+                        todoList.append(tempList)
+                    tempScoreList=standaradizing(positive(todoList,[0,1,0]))
+                    ScoreList=getScore(tempScoreList,getWeight(tempScoreList))
+                    i=0
+                    while(i<len(ScoreList)):
+                        writeInDict(findList[i]['user_id'],findList[i]['case_id'],'firstConsScore',ScoreList[i])
+                        i+=1
+        # info_json = json.dumps(info_dict,sort_keys=False, indent=4, separators=(',', ': '),ensure_ascii=False)
+        #
+        # print(type(info_json))
+        # f = open('E:\\possiBC\\deepData\\TopsisProcess.json', 'w')
+        # f.write(info_json)
+
+def algorthmJudge():
+    with open('./preData/studentAlgorthmCapabilityData.json','r',encoding='gbk')as fp:
+        readyList=[]
+        json_data = json.load(fp)
+        for key in info_dict:
+            caseList=list(info_dict[key])
+            for case in caseList:
+                if(case['case_id'] not in readyList):
+                    readyList.append(case['case_id'])
+                    findList=findCase('./preData/studentAlgorthmCapabilityData.json',case['case_id'])
+                    todoList=[]
+                    for element in findList:
+                        tempList=[]
+                        tempList.append(element['final_score'])
+                        tempList.append(element['programLines'])
+                        tempList.append(float(element['timecost'])*10000000)
+                        tempList.append(element['memorycost'])
+                        todoList.append(tempList)
+                    tempScoreList=standaradizing(positive(todoList,[1,0,0,0]))
+                    ScoreList=getScore(tempScoreList,getWeight(tempScoreList))
+                    i=0
+                    while(i<len(ScoreList)):
+                        writeInDict(findList[i]['user_id'],findList[i]['case_id'],'algorthmScore',ScoreList[i])
+                        i+=1
         info_json = json.dumps(info_dict,sort_keys=False, indent=4, separators=(',', ': '),ensure_ascii=False)
 
         print(type(info_json))
         f = open('E:\\possiBC\\deepData\\TopsisProcess.json', 'w')
         f.write(info_json)
+
+def writeInDict(user_id,case_id,name,Score):
+    for key in info_dict:
+        if(key==user_id):
+            for case in info_dict[key]:
+                if(case['case_id']==case_id):
+                    case[name]=Score
 
 def positive(todoList,judgeList):
     i=0
@@ -56,13 +211,100 @@ def standaradizing(todoList):
     while(i<len(todoList)):
         j=0
         while(j<len(todoList[0])):
-            todoList[i][j]/=tempList[j]
-            todoList[i][j]=round(todoList[i][j],4)
+            if(tempList[j]==0):
+                todoList[i][j]=0
+            else:
+                todoList[i][j]/=tempList[j]
+                todoList[i][j]=round(todoList[i][j],4)
             j+=1
         i+=1
     return todoList
 
-# def getPureScore():
+def getScore(todoList,weightList):
+    i=0
+    MAXlist=[]
+    MINlist=[]
+    while(i<len(todoList[0])):
+        j=0
+        MAX=todoList[0][i]
+        MIN=todoList[0][i]
+        while(j<len(todoList)):
+            if(todoList[j][i]>MAX):
+                MAX=todoList[j][i]
+            if(todoList[j][i]<MIN):
+                MIN=todoList[j][i]
+            j+=1
+        MAXlist.append(MAX)
+        MINlist.append(MIN)
+        i+=1
+    DplusList=[]
+    DminusList=[]
+    i=0
+    while(i<len(todoList)):
+        j=0
+        SUMplus=0
+        SUMminus=0
+        while(j<len(todoList[0])):
+            SUMplus+=pow(MAXlist[j]-todoList[i][j],2)*weightList[j]
+            SUMminus+=pow(MINlist[j]-todoList[i][j],2)*weightList[j]
+            j+=1
+        SUMplus=pow(SUMplus,0.5)
+        SUMminus =pow(SUMminus, 0.5)
+        DplusList.append(SUMplus)
+        DminusList.append(SUMminus)
+        i+=1
 
-# print(positive([[1,2,3],[4,5,6],[7,8,9]],[0,0,1]))
-# print(standaradizing([[89,1],[60,3],[74,2],[99,0]]))
+    pureScoreList=[]
+    j=0
+    while(j<len(DplusList)):
+        if((DplusList[j]+DminusList[j])!=0):
+            pureScoreList.append(round(DminusList[j]/(DplusList[j]+DminusList[j]),4))
+        else:
+            pureScoreList.append(0)
+        j+=1
+    ScoreList=[]
+    j=0
+    while(j<len(pureScoreList)):
+        if(sum(pureScoreList)!=0):
+            ScoreList.append(round(round(pureScoreList[j]/sum(pureScoreList),4)*100,2))
+        else:
+            ScoreList.append(0)
+        j+=1
+    return ScoreList
+
+def getWeight(todoList):
+    metadata={}
+    i=0
+    while(i<len(todoList[0])):
+        j=0
+        metadata[chr(ord('a')+i)]=[]
+        while(j<len(todoList)):
+            metadata[chr(ord('a') + i)].append(todoList[j][i])
+            j+=1
+        i+=1
+    # metadata = {'a': [0, 2, 3, 4, 5], 'b': [1, 2, 3, 1, 5], 'c': [2, 3, 2, 5, 2]}
+    metadata = pd.DataFrame(metadata)
+    # metadata={}
+    # 去除空值的记录
+    metadata.dropna()
+    weights = centropyWeightMethodCalculateWeight(metadata)  # 调用cal_weight
+    weights.index = metadata.columns
+    weights.columns = ['weight']
+    # print(weights.to_dict('list')['weight'])
+    return weights.to_dict('list')['weight']
+
+info_dict={}
+dataPre()
+debugJudge()
+firstConsJudge()
+algorthmJudge()
+
+
+# tempAns=positive([[89,2],[60,0],[74,1],[99,3]],[1,0])
+# print(tempAns)
+# tempAns=standaradizing(tempAns)
+# print(tempAns)
+# tempAns=getScore(tempAns,getWeight(tempAns))
+# print(tempAns)
+
+
